157cf063e99d57570081ac5fde66393bbad35cfc
Yusuf CEYLAN <yusufff91@gmail.com>
2023-09-10 02:49:31 +0300

    first commit

---
+++ Models/Candidate.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage ="E-mail is required.")]
+        public String? Email { get; set; }=String.Empty;
+
+        [Required(ErrorMessage ="First name is required.")]
+        public String? FirstName { get; set; }=String.Empty;
+
+        [Required(ErrorMessage ="Last name is required.")]
+        public String? LastName { get; set; }=String.Empty;
+
+        [Required(ErrorMessage ="Full name is required.")]
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        
+        [Required(ErrorMessage ="Please select course.")]
+        public String? SelectedCourse { get; set; }=String.Empty;
+        public DateTime ApplyAt { get; set; }
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
